70096b1e29c58aa82116989ef55dc1b0d485bfba
amodaresi <amodaresi@gmail.com>
2020-05-01 23:22:51 +0430

    updated to the newest implementation of tfa.addons

--- custom_utils.py
+++ custom_utils.py
@@ -1,86 +1,103 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
-from tensorflow.keras.metrics import Metric
-import numpy as np
 
-class SparseMatthewsCorrelationCoefficient(Metric):
+from tensorflow_addons.utils.types import AcceptableDTypes, FloatTensorLike
+from typeguard import typechecked
+
+
+@tf.keras.utils.register_keras_serializable(package="Addons")
+class SparseMatthewsCorrelationCoefficient(tf.keras.metrics.Metric):
     """Computes the Matthews Correlation Coefficient.
     The statistic is also known as the phi coefficient.
-    The Matthews correlation coefficient (MCC) is used in 
-    machine learning as a measure of the quality of binary 
-    and multiclass classifications. It takes into account 
-    true and false positives and negatives and is generally 
-    regarded as a balanced measure which can be used even 
-    if the classes are of very different sizes. The correlation 
-    coefficient value of MCC is between -1 and +1. A 
-    coefficient of +1 represents a perfect prediction, 
-    0 an average random prediction and -1 an inverse 
-    prediction. The statistic is also known as 
+    The Matthews correlation coefficient (MCC) is used in
+    machine learning as a measure of the quality of binary
+    and multiclass classifications. It takes into account
+    true and false positives and negatives and is generally
+    regarded as a balanced measure which can be used even
+    if the classes are of very different sizes. The correlation
+    coefficient value of MCC is between -1 and +1. A
+    coefficient of +1 represents a perfect prediction,
+    0 an average random prediction and -1 an inverse
+    prediction. The statistic is also known as
     the phi coefficient.
     MCC = (TP * TN) - (FP * FN) /
           ((TP + FP) * (TP + FN) * (TN + FP ) * (TN + FN))^(1/2)
-    Args:
-       num_classes : Number of unique classes in the dataset.
-    Returns:
-       mcc : float (the Matthews correlation coefficient)
     Usage:
     ```python
-    actuals = tf.constant([[1, 0, 1], [0, 1, 0]],
-             dtype=tf.int32)
-    preds = tf.constant([[1, 0, 0],[0, 1, 1]],
-             dtype=tf.int32)             
+    actuals = tf.constant([[1.0], [1.0], [1.0], [0.0]],
+             dtype=tf.float32)
+    preds = tf.constant([[1.0], [0.0], [1.0], [1.0]],
+             dtype=tf.float32)
     # Matthews correlation coefficient
-    mcc = MatthewsCorrelationCoefficient(num_classes=3)
-	output.update_state(actuals, preds)
-	print('MCC:', output.result().numpy())
-	# mcc : 
+    mcc = MatthewsCorrelationCoefficient(num_classes=1)
+    mcc.update_state(actuals, preds)
+    print('Matthews correlation coefficient is:',
+    mcc.result().numpy())
+    # Matthews correlation coefficient is : -0.33333334
     ```
     """
-    
-    def __init__(self, 
-                 num_classes, 
-                 name='Sparse_Matthews_Correlation_Coefficient', 
-                 dtype=tf.int32):
-        super(SparseMatthewsCorrelationCoefficient, self).__init__(name=name, dtype=dtype)
+
+    @typechecked
+    def __init__(
+        self,
+        num_classes: FloatTensorLike,
+        name: str = "MatthewsCorrelationCoefficient",
+        dtype: AcceptableDTypes = None,
+        **kwargs
+    ):
+        """Creates a Matthews Correlation Coefficient instance.
+        Args:
+            num_classes : Number of unique classes in the dataset.
+            name: (Optional) String name of the metric instance.
+            dtype: (Optional) Data type of the metric result.
+            Defaults to `tf.float32`.
+        """
+        super().__init__(name=name, dtype=dtype)
         self.num_classes = num_classes
         self.true_positives = self.add_weight(
-            'true_positives',
+            "true_positives",
             shape=[self.num_classes],
-            initializer='zeros',
-            dtype=self.dtype)
+            initializer="zeros",
+            dtype=self.dtype,
+        )
         self.false_positives = self.add_weight(
-            'false_positives',
+            "false_positives",
             shape=[self.num_classes],
-            initializer='zeros',
-            dtype=self.dtype)
+            initializer="zeros",
+            dtype=self.dtype,
+        )
         self.false_negatives = self.add_weight(
-            'false_negatives',
+            "false_negatives",
             shape=[self.num_classes],
-            initializer='zeros',
-            dtype=self.dtype)
+            initializer="zeros",
+            dtype=self.dtype,
+        )
         self.true_negatives = self.add_weight(
-            'true_negatives',
+            "true_negatives",
             shape=[self.num_classes],
-            initializer='zeros',
-            dtype=self.dtype)
+            initializer="zeros",
+            dtype=self.dtype,
+        )
 
-    def update_state(self, y_true, y_pred):
+    # TODO: sample_weights
+    def update_state(self, y_true, y_pred, sample_weight=None):
         y_pred = tf.expand_dims(tf.argmax(y_pred, axis=1), -1)
-        y_true = tf.cast(y_true, tf.int32)
-        y_pred = tf.cast(y_pred, tf.int32)
+        y_true = tf.cast(y_true, dtype=self.dtype)
+        y_pred = tf.cast(y_pred, dtype=self.dtype)
+        
 
         true_positive = tf.math.count_nonzero(y_true * y_pred, 0)
+        # true_negative
+        y_true_negative = tf.math.not_equal(y_true, 1.0)
+        y_pred_negative = tf.math.not_equal(y_pred, 1.0)
+        true_negative = tf.math.count_nonzero(
+            tf.math.logical_and(y_true_negative, y_pred_negative), axis=0
+        )
         # predicted sum
         pred_sum = tf.math.count_nonzero(y_pred, 0)
         # Ground truth label sum
         true_sum = tf.math.count_nonzero(y_true, 0)
         false_positive = pred_sum - true_positive
         false_negative = true_sum - true_positive
-        true_negative = y_true.get_shape(
-        )[0] - true_positive - false_positive - false_negative
 
         # true positive state_update
         self.true_positives.assign_add(tf.cast(true_positive, self.dtype))
@@ -91,21 +108,21 @@ class SparseMatthewsCorrelationCoefficient(Metric):
         # true negative state_update
         self.true_negatives.assign_add(tf.cast(true_negative, self.dtype))
 
-
     def result(self):
-    	  # numerator
-    	  numerator1 = tf.cast(self.true_positives * self.true_negatives, self.dtype)
-    	  numerator2 = tf.cast(self.false_positives * self.false_negatives, self.dtype)
-    	  numerator = numerator1 - numerator2
-    	  # denominator
-    	  denominator1 = tf.cast(self.true_positives + self.false_positives, self.dtype)
-    	  denominator2 = tf.cast(self.true_positives + self.false_negatives, self.dtype)
-    	  denominator3 = tf.cast(self.true_negatives + self.false_positives, self.dtype)
-    	  denominator4 = tf.cast(self.true_negatives + self.false_negatives, self.dtype)
-    	  denominator = tf.math.sqrt(denominator1 * denominator2 * denominator3 * denominator4)
-    	  mcc = tf.math.divide_no_nan(numerator, denominator)
-
-    	  return mcc
+        # numerator
+        numerator1 = self.true_positives * self.true_negatives
+        numerator2 = self.false_positives * self.false_negatives
+        numerator = numerator1 - numerator2
+        # denominator
+        denominator1 = self.true_positives + self.false_positives
+        denominator2 = self.true_positives + self.false_negatives
+        denominator3 = self.true_negatives + self.false_positives
+        denominator4 = self.true_negatives + self.false_negatives
+        denominator = tf.math.sqrt(
+            denominator1 * denominator2 * denominator3 * denominator4
+        )
+        mcc = tf.math.divide_no_nan(numerator, denominator)
+        return mcc
 
     def get_config(self):
         """Returns the serializable config of the metric."""
@@ -113,15 +130,16 @@ class SparseMatthewsCorrelationCoefficient(Metric):
         config = {
             "num_classes": self.num_classes,
         }
-        base_config = super(SparseMatthewsCorrelationCoefficient, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+        base_config = super().get_config()
+        return {**base_config, **config}
 
     def reset_states(self):
         """Resets all of the metric state variables."""
-        self.true_positives.assign(np.zeros(self.num_classes), np.int32)
-        self.false_positives.assign(np.zeros(self.num_classes), np.int32)
-        self.false_negatives.assign(np.zeros(self.num_classes), np.int32)
-        self.true_negatives.assign(np.zeros(self.num_classes), np.int32)
+        self.true_positives.assign(tf.zeros((self.num_classes), self.dtype))
+        self.false_positives.assign(tf.zeros((self.num_classes), self.dtype))
+        self.false_negatives.assign(tf.zeros((self.num_classes), self.dtype))
+        self.true_negatives.assign(tf.zeros((self.num_classes), self.dtype))
+
 
 class PearsonCorr(tf.keras.metrics.Metric):
     def __init__(self, name='PearsonCorrelation', **kwargs):
